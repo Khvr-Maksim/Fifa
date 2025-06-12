7c0dcd3e84654cffeedd7182abe28e62c10a2916
Khvr-Maksim <havrov_maksim@mail.ru>
2025-06-12 21:51:11 +0300

    Add scss and html

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import injectHTML from 'vite-plugin-html-inject';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
     root:"./src",
@@ -7,6 +10,25 @@ export default defineConfig({
         open:true,
     },
     build:{
-        outDir:'./dist'
+        outDir:'./dist',
+        emptyOutDir:true,
+        rollupOptions:{
+            input:{
+                main: './src/index.html',
+                about: './src/about.html',
+            }
+        }
+
     },
+    plugins: [
+        ViteMinifyPlugin({}),
+        injectHTML(),
+        ViteImageOptimizer({
+        png: {
+            quality: 80,
+          },
+          jpg: {
+            quality: 80,
+          },
+    })]
 })
