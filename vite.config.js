891d5d2fc7195cbd3a31be6489c5dc351aeab211
Khvr-Maksim <havrov_maksim@mail.ru>
2025-06-15 11:54:07 +0300

    Added OVERVIEW-section

--- vite.config.js
+++ vite.config.js
@@ -4,31 +4,36 @@ import { ViteMinifyPlugin } from 'vite-plugin-minify';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
-    root:"./src",
-    server:{
-        port:3000,
-        open:true,
+  root: './src',
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: './dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './src/index.html',
+      },
     },
-    build:{
-        outDir:'./dist',
-        emptyOutDir:true,
-        rollupOptions:{
-            input:{
-                main: './src/index.html',
-                about: './src/about.html',
-            }
-        }
-
-    },
-    plugins: [
-        ViteMinifyPlugin({}),
-        injectHTML(),
-        ViteImageOptimizer({
-        png: {
-            quality: 80,
-          },
-          jpg: {
-            quality: 80,
-          },
-    })]
-})
+  },
+  plugins: [
+    injectHTML(),
+    ViteMinifyPlugin({
+      collapseWhitespace: true,
+      removeComments: true,
+      removeRedundantAttributes: true,
+      removeScriptTypeAttributes: true,
+      removeStyleLinkTypeAttributes: true,
+      minifyCSS: true,
+      minifyJS: true,
+      useShortDoctype: true,
+      sortClassName: true,
+    }),
+    ViteImageOptimizer({
+      png: { quality: 80 },
+      jpg: { quality: 80 },
+    }),
+  ],
+});
