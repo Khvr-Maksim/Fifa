b284e72a6d9bc8865744e0b28647d4559982bbf4
Khvr-Maksim <havrov_maksim@mail.ru>
2025-06-16 12:59:47 +0300

    Added ACCORDEON.js

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,10 @@ import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
 import {useInsightSlider} from './components/home/slider.js';
 import {useTestimonialsSlider} from './components/home/slider.js';
+import { initFooterAccordion } from './components/home/accordeon.js';
 
 useTheme();
 useBurger();
 useInsightSlider();
 useTestimonialsSlider();
+initFooterAccordion();
